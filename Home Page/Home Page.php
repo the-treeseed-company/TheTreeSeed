9a2c4f8c60d6e006af42ca847c6d4a710a8ae226
reyno-strategix <48205944+reyno-strategix@users.noreply.github.com>
2019-05-30 16:23:38 +0200

    First Website commits
    - I created the first page of the website

---
+++ Home Page/Home Page.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Tree Seed</title>
+
+            <!-- Defining the view port -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <!-- Javascript links -->
+                <!-- The main Javascript file -->
+                    <script type="text/javascript" src="Home Page.js"></script>
+
+            <!-- CSS links -->
+                <!-- The main CSS file -->
+                    <link rel = "stylesheet" type = "text/css" href = "Home Page.css" />
+
+            <!-- Boostrap links -->
+                <!-- Latest compiled and minified CSS -->
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+                <!-- jQuery library -->
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+                <!-- Popper JS -->
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+                <!-- Latest compiled JavaScript -->
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            
+            <!-- Page Spesific CSS -->
+                <style>
+
+                </style>
+
+            <!-- Before rendering Javascript -->
+                <script>
+
+                </script>
+        </head>
+        <body>
+            <div class="container-fluid">
+                <?php
+                    echo "My first PHP script!";
+                ?> 
+            </div>
+
+            
+            <!-- After redering Javascript -->
+                <script>
+
+                </script>
+        </body>
+    </html>
